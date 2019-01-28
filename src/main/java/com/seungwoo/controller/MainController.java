f919d0cd437b19e40f5b7950f903d9931cd13616
sonseungwoo <skyghy3342@naver.com>
2019-01-28 15:36:19 +0900

    코드 수정

--- src/main/java/com/seungwoo/controller/MainController.java
+++ src/main/java/com/seungwoo/controller/MainController.java
@@ -1,16 +1,11 @@
 package com.seungwoo.controller;
 
 import com.seungwoo.domain.Account;
-import com.seungwoo.service.AccountService;
-import com.seungwoo.service.AccountServiceImpl;
-import com.seungwoo.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.context.ServerSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-
 /**
  * Created by Leo.
  * User: ssw
@@ -28,25 +21,16 @@ import java.util.List;
  * Time: 10:15
  */
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
-    @Autowired
-    AccountService accountService;
+    private final ReactiveAuthenticationManager reactiveAuthenticationManager;
 
-    @Autowired
-    AccountServiceImpl accountServiceImpl;
-
-    @Autowired
-    TestService testService;
-
-    @Autowired
-    private ReactiveAuthenticationManager reactiveAuthenticationManager;
-
-    @Autowired
-    private ServerSecurityContextRepository contextRepository;
+    private final ServerSecurityContextRepository contextRepository;
 
     /**
      * reactive security
+     *
      * @param account
      * @param serverWebExchange
      * @return
@@ -55,42 +39,20 @@ public class MainController {
     public String login(@ModelAttribute Account account, ServerWebExchange serverWebExchange) {
         reactiveAuthenticationManager.authenticate(new UsernamePasswordAuthenticationToken(account.getUsername(), null, null))
                 .subscribe(authentication -> {
-                   SecurityContextImpl securityContext = new SecurityContextImpl();
-                   securityContext.setAuthentication(authentication);
-                   this.contextRepository.save(serverWebExchange, securityContext)
-                           .subscriberContext(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(securityContext)))
-                           .subscribe();
+                    SecurityContextImpl securityContext = new SecurityContextImpl();
+                    securityContext.setAuthentication(authentication);
+                    this.contextRepository.save(serverWebExchange, securityContext)
+                            .subscriberContext(ReactiveSecurityContextHolder.withSecurityContext(Mono.just(securityContext)))
+                            .subscribe();
                 });
-        /*Mono<UserDetails> userDetails = accountServiceImpl.findByUsername(account.getUsername());
-        System.out.println(userDetails.toString());
-        Authentication authentication = new UsernamePasswordAuthenticationToken("seungwoo1","0000", Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-        ReactiveSecurityContextHolder.getContext()
-                .switchIfEmpty(Mono.error(new IllegalStateException("ReactiveSecurityContext is empty")))
-                .map(SecurityContext::getAuthentication)
-                .map(Authentication::getName)
-                .subscriberContext(ReactiveSecurityContextHolder.withAuthentication(authentication));*/
         return "redirect:index";
     }
 
     /**
      * 기존 security
+     *
      * @return
      */
-    /*@GetMapping("/front")
-    public String login(@ModelAttribute Account account, HttpServletRequest request){
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken("seungwoo1", "0000", Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")));
-
-        UserDetails ckUserDetails = testService.loadUserByUsername("seungwoo1");
-        Authentication authentication = new UsernamePasswordAuthenticationToken("seungwoo1", ckUserDetails.getPassword(), ckUserDetails.getAuthorities());
-
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
-        HttpSession session = request.getSession(true);
-        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
-
-        return "index";
-    }*/
-
     @GetMapping("/front/all")
     @ResponseBody
     public String all() {
@@ -100,7 +62,6 @@ public class MainController {
 
     @GetMapping("/index")
     public String main() {
-        List<Account> list = accountService.findAll();
         System.out.println("===========index===========");
         return "index";
     }
@@ -127,7 +88,7 @@ public class MainController {
 
     @GetMapping("/authentication-failure")
     @ResponseBody
-    public String failure(){
+    public String failure() {
         return "실패";
     }
 }
