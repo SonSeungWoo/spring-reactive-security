b5ee76dbc38f161e4c7e0a0bbc3d79d012a96a76
sonseungwoo <skyghy3342@naver.com>
2019-02-10 16:14:16 +0900

    LogFilter

--- src/main/java/com/seungwoo/interceptor/LogFilter.java
+++ src/main/java/com/seungwoo/interceptor/LogFilter.java
@@ -2,10 +2,14 @@ package com.seungwoo.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Leo.
@@ -20,6 +24,9 @@ public class LogFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         long startTime = System.currentTimeMillis();
         String path = exchange.getRequest().getURI().getPath();
+        /*MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
+        Mono<MultiValueMap<String, String>> formData = exchange.getFormData();
+        Mono<MultiValueMap<String, Part>> multipartData = exchange.getMultipartData();*/
         //web handleInterceptorAdapter preHandle
         log.info("Serving '{}'", path);
         //return handleInterceptoprAdapter postHandle
